7f7112b7624080661c4098b3e68e6aaf3a93030d
nambb <nam.buiba@abs.vn>
2024-11-07 16:39:08 +0700

    first commit

---
+++ src/main/java/com/nambb/nina/NinaApplication.java
@@ -0,0 +1,13 @@
+package com.nambb.nina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nambb/nina/NinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nambb.nina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
